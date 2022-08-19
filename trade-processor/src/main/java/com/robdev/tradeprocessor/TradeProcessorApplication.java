0ac359315dc3fedcc54d722574d49c2f170c1e3a
Robert <rcgray306@gmail.com>
2022-08-19 20:22:32 +0100

    Initial commit.

---
+++ trade-processor/src/main/java/com/robdev/tradeprocessor/TradeProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.robdev.tradeprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeProcessorApplication.class, args);
+	}
+
+}
---
+++ trade-processor/src/test/java/com/robdev/tradeprocessor/TradeProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robdev.tradeprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
