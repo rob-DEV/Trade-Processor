d7ec4e3f79e2817726efd9d78bb39741e678a616
Robert <rcgray306@gmail.com>
2022-10-09 20:10:11 +0100

    Refactor processor and add spotless

--- trade-processor/src/main/java/com/robdev/tradeprocessor/service/TradeProcessor.java
+++ trade-processor/src/main/java/com/robdev/tradeprocessor/service/TradeProcessor.java
@@ -1,48 +1,71 @@
 package com.robdev.tradeprocessor.service;
 
 import com.robdev.tradeprocessor.config.KafkaProperties;
-import com.robdev.tradeprocessor.enrichment.Trade;
-import lombok.NonNull;
+import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.producer.ProducerRecord;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.ApplicationListener;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import reactor.core.Disposable;
+import reactor.core.Disposables;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.kafka.receiver.KafkaReceiver;
+import reactor.kafka.receiver.ReceiverRecord;
 import reactor.kafka.sender.KafkaSender;
 import reactor.kafka.sender.SenderRecord;
 import reactor.kafka.sender.SenderResult;
 
-import static com.robdev.tradeprocessor.enrichment.TradeEnrichmentFunctions.*;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import java.util.UUID;
 
 @Slf4j
 @RequiredArgsConstructor
-@Component
-public class TradeProcessor implements ApplicationListener<ApplicationReadyEvent> {
+@Service
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class TradeProcessor {
 
-    private final KafkaProperties kafkaProperties;
-    private final KafkaReceiver<String, String> kafkaReceiver;
-    private final KafkaSender<String, String> kafkaSender;
-    
-    @Override
-    public void onApplicationEvent(@NonNull final ApplicationReadyEvent event) {
-        Flux<ConsumerRecord<String, String>> kafkaFlux = kafkaReceiver.receiveAtmostOnce();
+    KafkaProperties kafkaProperties;
+    KafkaReceiver<String, String> kafkaReceiver;
+    KafkaSender<String, String> kafkaSender;
+    Disposable.Composite disposables = Disposables.composite();
 
-        Flux<Trade> enrichmentFlux = kafkaFlux
-                .map(COMPOSED_TRADE_ENRICHMENT_FUNCTIONS)
-                .flatMapIterable(s -> s)
-                .doOnError(err -> log.error("Enrichment Error: {}", err.getMessage()));
+    @PostConstruct
+    public void connect() {
+        disposables.add(
+                receive()
+                        .doOnError(error -> log.info( "Error in Kafka Receiver flow", error))
+                        .subscribe(s -> log.info("Ended subscription to Kafka Receiver"))
+        );
+    }
 
-        Flux<SenderResult<Integer>> senderFlux = kafkaSender.send(enrichmentFlux
-                        .map(mapToJson())
-                        .map(json -> SenderRecord.create(new ProducerRecord<>(kafkaProperties.getOutboundTopic(), json), 1)))
-                .log()
-                .doOnError(err -> log.info("Publishing Error {}", err.getMessage()));
+    @PreDestroy
+    public void disconnect() {
+        this.disposables.dispose();
+    }
 
-        enrichmentFlux.subscribe();
-        senderFlux.subscribe();
+    public Flux<SenderResult<UUID>> receive() {
+        return kafkaReceiver.receive()
+                .doOnNext(record -> log.info("Received record with key={}", record.key()))
+                .flatMap(this::process);
+    }
+
+    public Flux<SenderResult<UUID>> process(ReceiverRecord<String, String> record) {
+        var senderRecord = SenderRecord.create(
+                new ProducerRecord<>(
+                        kafkaProperties.getOutboundTopic(),
+                        null,
+                        record.key(),
+                        record.value(),
+                        null
+                ),
+                UUID.randomUUID()
+        );
+
+        log.info("Sending message with key={}", senderRecord.key());
+        return kafkaSender.send(Mono.just(senderRecord));
     }
 }
